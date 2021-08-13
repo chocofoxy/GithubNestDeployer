e9baefd77da200d9d122a85fc128e49c865cff9c
sihamza <hamzus007jouini@gmail.com>
2021-08-14 00:17:09 +0100

    add next deployment

--- index.js
+++ index.js
@@ -18,5 +18,22 @@ app.post('/deploy-nest', (req, res) => {
     })
 })
 
+app.post('/deploy-next', (req, res) => {
+    exec('cd /workspace/tynass-backend && git pull && npm run build > /build-error.logs || true && pm2 restart dist/main.js',
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log('Redeployment failed:', Date.now())
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log('Redeployment successful:', Date.now())
+        }
+    );
+    console.log('Redeployment attempt:', Date.now())
+    res.status(200).json({
+        status: 'ok'
+    })
+})
+
 
 app.listen(8080)
