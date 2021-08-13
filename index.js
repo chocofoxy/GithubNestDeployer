12076ee98aed58d6046c4ecd5ebc828b43de4abe
sihamza <hamzus007jouini@gmail.com>
2021-08-13 22:47:11 +0100

    6th push

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const app = require('express')()
 const { exec } = require('child_process');
 
-app.post('/deploy-nest', (req,res) => {
-    exec('cd ~/tynass-backend && git pull && npm run build && pm2 restart dist/main.js');
-    console.log('Redeployment :', Date.now() )
-    res.status(200).json({ status : 'ok' })
+app.post('/deploy-nest', (req, res) => {
+    exec('cd ~/tynass-backend && git pull && npm run build && pm2 restart dist/main.js',
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        }
+    );
+    console.log('Redeployment :', Date.now())
+    res.status(200).json({
+        status: 'ok'
+    })
 })
 
 
