0b1e4f63a5b6f1b6a0a32bffae370f644ae19d05
sihamza <hamzus007jouini@gmail.com>
2021-08-13 21:55:13 +0100

    1st push

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')()
+const { exec } = require('child_process');
+
+
+
+
+
+app.all('deploy-nest', (req,res) => {
+    exec('cd ~/tynass-backend && npm run build && pm2 restart dist/main.js');
+})
+
+
+app.listen(8080)
