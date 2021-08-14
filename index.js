6d7dcf8467ab8d112f1183211ba11728288c695a
sihamza <hamzus007jouini@gmail.com>
2021-08-14 21:35:18 +0100

    add next deployment 2

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const app = require('express')()
 const { exec } = require('child_process');
 
 app.post('/deploy-nest', (req, res) => {
-    exec('cd /workspace/tynass-backend && git pull && npm run build > /build-error.logs || true && pm2 restart dist/main.js',
+    exec('cd /workspace/tynass-backend && git pull && npm install || true && npm run build > /build-error.logs || true && pm2 restart dist/main.js',
         (error, stdout, stderr) => {
             if (error) {
-                console.log('Redeployment failed:', Date.now())
+                console.log('Redeployment nest failed:', Date.now())
                 console.error(`exec error: ${error}`);
                 return;
             }
-            console.log('Redeployment successful:', Date.now())
+            console.log('Redeployment nest successful:', Date.now())
         }
     );
-    console.log('Redeployment attempt:', Date.now())
+    console.log('Redeployment nest attempt:', Date.now())
     res.status(200).json({
         status: 'ok'
     })
 })
 
 app.post('/deploy-next', (req, res) => {
-    exec('cd /workspace/tynass-backend && git pull && npm run build > /build-error.logs || true && pm2 restart dist/main.js',
+    exec('cd /workspace/tynass-office && git pull && npm install || true && npm run build > /build-error.logs || true && pm2 restart nextjs',
         (error, stdout, stderr) => {
             if (error) {
-                console.log('Redeployment failed:', Date.now())
+                console.log('Redeployment next failed:', Date.now())
                 console.error(`exec error: ${error}`);
                 return;
             }
-            console.log('Redeployment successful:', Date.now())
+            console.log('Redeployment next successful:', Date.now())
         }
     );
-    console.log('Redeployment attempt:', Date.now())
+    console.log('Redeployment next attempt:', Date.now())
     res.status(200).json({
         status: 'ok'
     })
